f891c7d009ebf3f7030ee05707ddf96eb57e2476
Your Name <you@example.com>
2022-05-08 04:17:44 +0300

    new aws url

--- src/components/home/homeService.ts
+++ src/components/home/homeService.ts
@@ -4,7 +4,7 @@ export default {
 
   getDaelyRates: (day:Date): Promise<{ rates: DcRate[] }> => {
     let dayParam= day.toISOString().split('T')[0];
-    return fetch(`http://brandefenderassignmentapi-env.eba-4qn32pkk.us-east-1.elasticbeanstalk.com/api/getDayRates/${dayParam}`)
+    return fetch(`http://brandefenderassignmentapi.us-east-1.elasticbeanstalk.com/api/getDayRates/${dayParam}`)
       .then(response => {
         if (!response.ok) {
           throw new Error(response.statusText)
